0b8b1b80256938127c2f07cc3f7020be467387af
alex-cres <alexrealinho@gmail.com>
2019-03-08 00:10:14 +0000

    There are some incongruencias na grammar, os castings

--- Compiler/SyntaxAnalyser.c
+++ Compiler/SyntaxAnalyser.c
@@ -27,14 +27,14 @@ int instruction(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme)
 /*
 <instruction> -- > <declaration>
 		| <attribution>
-		| <if_struct> //missing
+		| <if_struct>
 		| <looper> //missing
-		| <comment> //missing
+		| <comment>
 		| <for> //missing
 		| <scan> //missing
 		| <print> //missing
-		| <function> //missing
-		| <call_function> //missing
+		| <function> 
+		| <call_function>
 		| <return> //missing
 		| <continue> //missing
 		| <break> //missing
@@ -42,7 +42,7 @@ int instruction(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme)
 		| <fscan> //missing
 		| <fscanEnder> //missing
 		| <fscanSize> //missing
-		| EOF //missing
+		| EOF 
 		*/
 	
 	switch (nextToken)
@@ -50,7 +50,12 @@ int instruction(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme)
 	case RESERVED_IF:
 		nextToken=reservedIf(file,nextToken,nextChar,nextLexeme);
 		break;
-		
+	case RESERVED_COMMENT:
+		nextToken = lex(file, nextChar, nextLexeme);//gets next term
+		break;
+	case RESERVED_CALL_FUNCTION:
+		nextToken = callFunction(file, nextToken, nextChar, nextLexeme);
+		break;
 	case IDENTIFIER: //attribution
 		nextToken = attribution(file,nextToken,nextChar,nextLexeme);
 		break;
@@ -58,6 +63,7 @@ int instruction(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme)
 	case IDN_BOOL: case IDN_NUMBER:	case IDN_DECIMAL: case IDN_CHAR: case IDN_VOID:	case IDN_STRING:
 		nextToken=declaration(file, nextToken, nextChar, nextLexeme);
 		break;
+	case EOF: break;
 	default:syntaxError(nextLexeme); break;
 	}
 	printf("Exiting <INSTRUCTION>\n");
@@ -75,7 +81,7 @@ int reservedIf(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme) 
 	nextToken = lex(file, nextChar, nextLexeme);//gets next term
 	if (nextToken == OPEN_PARENTESIS) {
 		nextToken = lex(file, nextChar, nextLexeme);//gets next term
-		nextToken = boolexp(file, nextToken, nextChar, nextLexeme);
+		nextToken = boolstruct(file, nextToken, nextChar, nextLexeme);
 		if (nextToken == CLOSE_PARENTESIS) {
 			nextToken = lex(file, nextChar, nextLexeme);//gets next term
 			if (nextToken == POINT) {
@@ -134,54 +140,109 @@ int reservedIf(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme) 
 	return nextToken;
 }
 
-int boolexp(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme) {
-	printf("Entering <BOOL>\n");
+int params(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme)
+{
+	printf("Entering <PARAMS>\n");
 	/*
-	<bool> --> <exp> < <exp>//missing
-		| <exp> > <exp>//missing
-		| <exp> >= <exp>//missing
-		| <exp> <= <exp>//missing
-		| <exp> = <exp>//missing
-		| <exp> != <exp>//missing
-		| !(<bool>)
-		| (<bool>)
-		| <bool> & <bool>//missing
-		| <bool> | <bool>//missing
-		| <bool> X <bool>//missing
+	<params> --> <type_identifier> <var>
+			| <type_identifier> <var>, <params> 
+			*/
+	do {
+		if (nextToken == IDN_BOOL || nextToken == IDN_NUMBER || nextToken == IDN_DECIMAL || nextToken == IDN_CHAR || nextToken == IDN_STRING) {
+			nextToken = lex(file, nextChar, nextLexeme);//gets next term
+			if (nextToken==IDENTIFIER) {
+				nextToken = lex(file, nextChar, nextLexeme);//gets next term
+			}
+			else {
+				syntaxError(nextLexeme);
+			}
+		}
+		else if (nextToken == IDN_VOID) {
+			errorColor();
+			printf("ERROR:At Line %i : SYNTAX ERROR VOID PARAMS ARE FORBIDDEN, %s", lineNumber, nextLexeme);
+			normalColor();
+			exit(ERROR_SYNTAX_ERROR_VOID_PARAM);
+		}
+		else {
+			syntaxError(nextLexeme);
+		}
+	} while (nextToken==COMMA) ;
+		printf("Exiting <PARAMS>\n");
+	return nextToken;
+}
+
+int boolstruct(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme) {
+	printf("Entering <BOOLSTRUCT>\n");
+	/*
+	<boolstruct> -->  !(<boolstruct>)
+		| (<boolstruct>)
+		| <boolexp>
+		| <boolstruct> & <boolstruct>
+		| <boolstruct> | <boolstruct>
+		| <boolstruct> X <boolstruct>
 		| True
 		| False
 		| <Bcasting>//missing
 	*/
-	
-	if (nextToken == OP_NOT) {
-		nextToken = lex(file, nextChar, nextLexeme);//gets next term
-		if (nextToken == OPEN_PARENTESIS) {
+	switch (nextToken) {
+	case OP_NOT:	
 			nextToken = lex(file, nextChar, nextLexeme);//gets next term
-			nextToken = boolexp(file, nextToken, nextChar, nextLexeme);
-			if (nextToken == CLOSE_PARENTESIS) {
+	case OPEN_PARENTESIS:
+			if (nextToken == OPEN_PARENTESIS) {
 				nextToken = lex(file, nextChar, nextLexeme);//gets next term
-				printf("Exiting <BOOL>\n");
-				return nextToken;
+				nextToken = boolexp(file, nextToken, nextChar, nextLexeme);
+				if (nextToken == CLOSE_PARENTESIS) {
+					nextToken = lex(file, nextChar, nextLexeme);//gets next term
+					printf("Exiting <BOOLSTRUCT>\n");
+					return nextToken;
+				}
+				else {
+					syntaxError(nextLexeme);
+				}
 			}
-		}
-	}
-	if (nextToken == OPEN_PARENTESIS) {
-		nextToken = lex(file, nextChar, nextLexeme);//gets next term
-		nextToken = boolexp(file, nextToken, nextChar, nextLexeme);
-		if (nextToken == CLOSE_PARENTESIS) {
+			else {
+				syntaxError(nextLexeme);
+			}
+			break;
+	case RESERVED_BOOL_TRUE: case RESERVED_BOOL_FALSE:
 			nextToken = lex(file, nextChar, nextLexeme);//gets next term
-			printf("Exiting <BOOL>\n");
+			printf("Exiting <BOOLSTRUCT>\n");
 			return nextToken;
+		break;
+	default:
+		nextToken = boolstruct(file, nextToken, nextChar, nextLexeme);
+		if (nextToken == OP_AND || nextToken == OP_OR || nextToken == OP_XOR) {
+			nextToken = lex(file, nextChar, nextLexeme);//gets next term
+			nextToken = boolstruct(file, nextToken, nextChar, nextLexeme);
 		}
-	}
-	if (nextToken == RESERVED_BOOL_TRUE || nextToken == RESERVED_BOOL_FALSE) {
-		nextToken = lex(file, nextChar, nextLexeme);//gets next term
-		printf("Exiting <BOOL>\n");
+		printf("Exiting <BOOLSTRUCT>\n");
 		return nextToken;
+		break;
 	}
+	printf("Exiting <BOOLSTRUCT>\n");
+	return nextToken;
+}
+int boolexp(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme) {
+	printf("Entering <BOOLEXP>\n");
+	/*
+	<boolexp> --> <exp> < <exp>
+		| <exp> > <exp>
+		| <exp> >= <exp>
+		| <exp> <= <exp>
+		| <exp> = <exp>
+		| <exp> != <exp>
+		
+	*/
 	
+	
+	nextToken = exp(file, nextToken, nextChar, nextLexeme);
+	if (nextToken==OP_EQUAL|| nextToken == OP_MINOR_EQUAL || nextToken == OP_MINOR || nextToken == OP_BIGGER || nextToken == OP_BIGGER_EQUAL || nextToken == OP_NOT_EQUAL)
+	{
+		nextToken = lex(file, nextChar, nextLexeme);//gets next term
+		nextToken = exp(file, nextToken, nextChar, nextLexeme);
+	}
 
-	printf("Exiting <BOOL>\n");
+	printf("Exiting <BOOLEXP>\n");
 	return nextToken;
 }
 int declaration(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme) {
@@ -209,12 +270,101 @@ int functionDec(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme)
 
 	/*
 	<functionDec> -- > <function_name>(<params>).Do(<instructionList>)
+	<functionDec> -- > <function_name>().Do(<instructionList>)
 
 	*/
+	if (nextToken==IDENTIFIER_FUNCTION) {
+		nextToken = lex(file, nextChar, nextLexeme);//gets next term
+		if (nextToken == OPEN_PARENTESIS) {
+			nextToken = lex(file, nextChar, nextLexeme);//gets next term
+			if (nextToken == CLOSE_PARENTESIS) {
+				nextToken = lex(file, nextChar, nextLexeme);//gets next term
+				if (nextToken == POINT) {
+					nextToken = lex(file, nextChar, nextLexeme);//gets next term
+					if (nextToken == RESERVED_DO) {
+						nextToken = lex(file, nextChar, nextLexeme);//gets next term
+						if (nextToken == OPEN_PARENTESIS) {
+							nextToken = lex(file, nextChar, nextLexeme);//gets next term
+							nextToken = instructionList(file, nextToken, nextChar, nextLexeme);
+							if (nextToken == CLOSE_PARENTESIS) {
+								nextToken = lex(file, nextChar, nextLexeme);//gets next term
+							}
+							else {
+								syntaxError(nextLexeme);
+							}
+						}
+						else {
+							syntaxError(nextLexeme);
+						}
+					}
+					else {
+						syntaxError(nextLexeme);
+					}
+				}
+				else {
+					syntaxError(nextLexeme);
+				}
+			}
+			else {
+				nextToken = params(file, nextToken, nextChar, nextLexeme);
+				if (nextToken == CLOSE_PARENTESIS) {
+					nextToken = lex(file, nextChar, nextLexeme);//gets next term
+					if (nextToken == POINT) {
+						nextToken = lex(file, nextChar, nextLexeme);//gets next term
+						if (nextToken == RESERVED_DO) {
+							nextToken = lex(file, nextChar, nextLexeme);//gets next term
+							if (nextToken == OPEN_PARENTESIS) {
+								nextToken = lex(file, nextChar, nextLexeme);//gets next term
+								nextToken = instructionList(file, nextToken, nextChar, nextLexeme);
+								if (nextToken == CLOSE_PARENTESIS) {
+									nextToken = lex(file, nextChar, nextLexeme);//gets next term
+								}
+								else {
+									syntaxError(nextLexeme);
+								}
+							}
+							else {
+								syntaxError(nextLexeme);
+							}
+						}
+						else {
+							syntaxError(nextLexeme);
+						}
+					}
+					else {
+						syntaxError(nextLexeme);
+					}
+				}
+				else {
+					syntaxError(nextLexeme);
+				}
+			}
+		}
+		else {
+		syntaxError(nextLexeme);
+	}
+		
+	}
 	
 	
 	printf("Exiting <FUNCTIONDEC>\n");
 	return nextToken;
+}
+
+int params_call(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme)
+{
+	printf("Entering <PARAMSCALL>\n");
+	/*
+<params_call> -->  <exp>
+			| <exp>, <params_call>
+	*/
+	nextToken = exp(file, nextToken, nextChar, nextLexeme);
+	while (nextToken == COMMA) {
+		nextToken = exp(file, nextToken, nextChar, nextLexeme);
+		nextToken = lex(file, nextChar, nextLexeme);
+	}
+	printf("Exiting <PARAMSCALL>\n");
+	return nextToken;;
 }
 
 int declareExp(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme) {
@@ -263,6 +413,53 @@ int attribution(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme)
 	printf("Exiting <ATTRIBUTION>\n");
 	return nextToken;
 }
+int callFunction(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme) {
+	printf("Entering <CALLFUNCTION>\n");
+	/*
+<call_function> --> F.<function_name>(<params_call>)
+				| F.<function_name>()
+	*/
+	if (nextToken == RESERVED_CALL_FUNCTION) {
+		nextToken = lex(file, nextChar, nextLexeme);//gets next term
+		if (nextToken == POINT) {
+			nextToken = lex(file, nextChar, nextLexeme);//gets next term
+			if (nextToken == IDENTIFIER_FUNCTION) {
+				nextToken = lex(file, nextChar, nextLexeme);//gets next term
+				if (nextToken == OPEN_PARENTESIS) {
+					nextToken = lex(file, nextChar, nextLexeme);//gets next term
+					if (nextToken == CLOSE_PARENTESIS) {
+						nextToken = lex(file, nextChar, nextLexeme);//gets next term
+					}
+					else {
+						nextToken = params_call(file, nextToken, nextChar, nextLexeme);
+						if (nextToken == CLOSE_PARENTESIS) {
+							nextToken = lex(file, nextChar, nextLexeme);//gets next term
+						}
+						else {
+							syntaxError(nextLexeme);
+						}
+					}
+					
+				}
+				else {
+					syntaxError(nextLexeme);
+				}
+			}
+			else {
+				syntaxError(nextLexeme);
+			}
+		}
+		else {
+			syntaxError(nextLexeme);
+		}
+	}
+	else {
+		syntaxError(nextLexeme);
+	}
+
+	printf("Exiting <CALLFUNCTION>\n");
+	return nextToken;
+}
 
 int exp(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme) {
 	printf("Entering <EXP>\n");
@@ -300,11 +497,16 @@ int factor(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme){
 	/*
 	<factor> -- > (<exp>)
 			| <var>
-			| <number_type>//missing
+			| +<number>
+			| -<number>
+			| +<decimal>
+			| -<decimal>
+			| <number>
 			| <decimal>
 			| <string>
 			| <char>
-			| <call_function>//missing
+			| <boolstruct>
+			| <call_function>
 	*/
 	printf("Entering <FACTOR>\n");
 	if (nextToken == IDENTIFIER || nextToken == LITERAL_CHAR || nextToken == LITERAL_STRING 
@@ -315,12 +517,15 @@ int factor(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme){
 	}
 	else if (nextToken == OP_ADD || nextToken == OP_MINUS ) {
 		nextToken = lex(file, nextChar, nextLexeme);//gets next term
-		if (nextToken==LITERAL_DECIMAL|| nextToken == LITERAL_NUMBER) {
+		if (nextToken==LITERAL_DECIMAL || nextToken == LITERAL_NUMBER) {
 			nextToken = lex(file, nextChar, nextLexeme);//gets next term
 		}
 		else {
 			syntaxError(nextLexeme);
 		}
+	}
+	else if (nextToken==RESERVED_CALL_FUNCTION) {
+		nextToken = callFunction(file, nextToken, nextChar, nextLexeme);
 	}
 	else {
 		if (nextToken == OPEN_PARENTESIS) {
@@ -334,7 +539,7 @@ int factor(FILE* file, int nextToken, NextChar* nextChar, char* nextLexeme){
 
 		}
 		else {
-			syntaxError(nextLexeme);
+			nextToken = boolstruct(file, nextToken, nextChar, nextLexeme);
 		}
 	}
 	printf("Exiting <FACTOR>\n");
